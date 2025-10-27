91750579b8ce7162c5d1593d1929bd4778bd5279
Slade13567 <shreyashari07@gmail.com>
2025-10-27 22:17:38 +0530

    Added Render domain to ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)z^cn+160daj$!!qj%%d569dkwou_d)$+00i#z$_g-2dm&22js'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
